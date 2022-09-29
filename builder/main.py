df727da57fc2e2733f68a01fd0a3583bfec65c8d
Andy Kuny <andrew.kuny@gsa.gov>
2022-09-29 09:26:07 -0400

    Change website to target_url

--- builder/main.py
+++ builder/main.py
@@ -20,21 +20,21 @@ if os.path.exists(todays_snapshot_path) == False:
     dap_df.to_csv(todays_snapshot_path + '/dap.csv', index=False)
 
 # normalize columns
-gov_df = gov_df.rename(columns={'Domain Name': 'website', 'Domain Type': 'branch', 'Agency': 'agency', 'Organization': 'bureau'})
-gov_df['base_domain'] = gov_df['website']
+gov_df = gov_df.rename(columns={'Domain Name': 'target_url', 'Domain Type': 'branch', 'Agency': 'agency', 'Organization': 'bureau'})
+gov_df['base_domain'] = gov_df['target_url']
 gov_df['source_list_federal_domains'] = 'TRUE'
 
-pulse_df = pulse_df.rename(columns= {'Domain': 'website', 'Base Domain': 'base_domain', 'Agency': 'agency'})
+pulse_df = pulse_df.rename(columns= {'Domain': 'target_url', 'Base Domain': 'base_domain', 'Agency': 'agency'})
 pulse_df['source_list_pulse'] = 'TRUE'
 
-dap_df = dap_df.rename(columns={'domain': 'website'})
+dap_df = dap_df.rename(columns={'domain': 'target_url'})
 dap_df['source_list_dap'] = 'TRUE'
 
 # strip out 'Federal - ' leading string from domain type column for .gov data
 gov_df['branch'] = gov_df['branch'].map(lambda x: x.lstrip('Federal - '))
 
 # combine all URLs into one column
-url_series = pd.concat([gov_df['website'], pulse_df['website'], dap_df['website'], additional_data['website']])
+url_series = pd.concat([gov_df['target_url'], pulse_df['target_url'], dap_df['target_url'], additional_data['target_url']])
 url_df = pd.DataFrame(url_series)
 
 # remove duplicates
@@ -45,13 +45,13 @@ ignore_df = pd.read_csv(config['ignore_list_path'])
 ignore_series = ignore_df['URL begins with:']
 
 for string in ignore_series:
-    url_df = url_df[~url_df['website'].str.startswith(string)]
+    url_df = url_df[~url_df['target_url'].str.startswith(string)]
 
 # merge data back in
-url_df = url_df.merge(gov_df, on='website', how='left')
-url_df = url_df.merge(pulse_df, on='website', how='left')
-url_df = url_df.merge(dap_df, on='website', how='left')
-url_df = url_df.merge(additional_data, on='website', how='left')
+url_df = url_df.merge(gov_df, on='target_url', how='left')
+url_df = url_df.merge(pulse_df, on='target_url', how='left')
+url_df = url_df.merge(dap_df, on='target_url', how='left')
+url_df = url_df.merge(additional_data, on='target_url', how='left')
 url_df = url_df.fillna('')
 
 url_df['agency'] = url_df['agency_x'].astype(str) + '' + url_df['agency_y'].astype(str)
@@ -64,7 +64,7 @@ url_df['source_list_dap'] = url_df['source_list_dap'].map(lambda x: 'FALSE' if x
 url_df['source_manually_added'] = url_df['source_manually_added'].map(lambda x: 'FALSE' if x == '' else x)
 
 # get relevant subset
-url_df = url_df[['website', 'base_domain', 'branch', 'agency', 'bureau', 'source_list_federal_domains', 'source_list_pulse', 'source_list_dap', 'source_manually_added']]
+url_df = url_df[['target_url', 'base_domain', 'branch', 'agency', 'bureau', 'source_list_federal_domains', 'source_list_pulse', 'source_list_dap', 'source_manually_added']]
 
 # load agency and bureau reference data
 omb_df = csv_to_df(config['omb_source_url'])
@@ -87,7 +87,7 @@ url_df['bureau_code'] = url_df['bureau_code'].map(lambda x: round_float(x))
 url_df['url'] = '---'
 
 # reorder columns
-url_df = url_df[['website', 'base_domain', 'url', 'branch', 'agency', 'agency_code', 'bureau', 'bureau_code', 'source_list_federal_domains', 'source_list_pulse', 'source_list_dap', 'source_manually_added']]
+url_df = url_df[['target_url', 'base_domain', 'url', 'branch', 'agency', 'agency_code', 'bureau', 'bureau_code', 'source_list_federal_domains', 'source_list_pulse', 'source_list_dap', 'source_manually_added']]
 
 # write list to csv
 url_df.to_csv(config['target_url_list_path'], index=False)
