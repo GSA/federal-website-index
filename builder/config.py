03809ef8cf35352eb39aef5e0b7ff670e637c4a1
Andy Kuny <andrew.kuny@gsa.gov>
2022-11-22 13:44:38 -0500

    Change pulse list URL in config

--- builder/config.py
+++ builder/config.py
@@ -6,7 +6,7 @@ dirname = os.path.dirname(__file__)
 
 config = {
     'gov_source_url': 'https://raw.githubusercontent.com/cisagov/dotgov-data/main/current-federal.csv',
-    'pulse_source_url': 'https://raw.githubusercontent.com/GSA/federal-website-index/main/data/snapshots/1-13-22/pulse-subdomains-snapshot-06-08-2020-https.csv',
+    'pulse_source_url': 'https://raw.githubusercontent.com/GSA/data/master/dotgov-websites/pulse-subdomains-snapshot-06-08-2020-https.csv',
     'dap_source_url': 'https://analytics.usa.gov/data/live/sites-extended.csv',
     'todays_snapshot_path': os.path.join(dirname, '../data/snapshots/' + datetime.now().strftime('%Y-%m-%d')),
     'additional_data_path': os.path.join(dirname, '../data/dataset/other-websites.csv'),
